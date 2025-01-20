bbbf5584e7fe1c64ba35180927d506c46935882f
Bannu <bharadvajnanepalli7@gmail.com>
2025-01-20 20:33:29 +0530

    Donar request screen changes are updated by Bharadvaj
    Modified   static/css/common.css
    Modified   static/js/common.js
    Modified   static/js/donarList.js
    Modified   templates/hospital/donar_list.html
    Modified   templates/hospital/home.html
    Modified   templates/hospital/seeker.html
    
    Signed-off-by: Bannu <bharadvajnanepalli7@gmail.com>

--- static/js/common.js
+++ static/js/common.js
@@ -1,33 +1,45 @@
-function openHome(path){
-    document.getElementById('homepageframe').src = '/'+path;
+function openHome(path) {
+    document.getElementById('homepageframe').src = '/' + path;
 }
 
-function validateLogin(){
-    if(document.forms[0].userType.value==''){
+function validateLogin() {
+    if (document.forms[0].userType.value == '') {
         alert('please select the user Type');
-    }  
-    else{
-        parent.document.getElementById('MainHeaderDiv').style.display="none";
-        parent.document.getElementById('mainFooterDiv').style.display="none";
     }
-    
+    else {
+        parent.document.getElementById('MainHeaderDiv').style.display = "none";
+        parent.document.getElementById('mainFooterDiv').style.display = "none";
+    }
+
 }
 
-function openHospitalHome(path){
-    document.getElementById('hospitalHomePage').src = '/'+path;
+function openHospitalHome(path) {
+    document.getElementById('hospitalHomePage').src = '/' + path;
 }
 
-function checkForError(){
-    if(document.getElementsByName('errorMsg')[0].value=="failed"){
+function checkForError() {
+    if (document.getElementsByName('errorMsg')[0].value == "failed") {
         alert('Login Failed');
-        parent.document.getElementById('MainHeaderDiv').style.display="block";
-        parent.document.getElementById('mainFooterDiv').style.display="block";
+        parent.document.getElementById('MainHeaderDiv').style.display = "block";
+        parent.document.getElementById('mainFooterDiv').style.display = "block";
     }
 }
 
-function commonaValidation(){
-    if(document.getElementById('seekerName').value=='' || document.getElementById('city').value=='' || document.getElementById('bloodgroup').value==''){
-        alert('Please select the mandatory fields');
+function validateSeekerFetchbtn() {
+    if (document.getElementById('seekerName').value == '' ){
+        alert('Please fill the seekerName details');
+        return false;
+    }
+    else if(document.getElementById('city').value == ''){
+        alert('Please fill the city details');
+        return false;
+    }
+    else if(document.getElementById('bloodgroup').value == ''){
+        alert('Please fill the Bloodgroup details');
+        return false;
+    }
+    else if(document.getElementById('seekerRequest').value == ''){
+        alert('Please fill the seekerRequest details');
         return false;
     }
     else{
--- static/js/donarList.js
+++ static/js/donarList.js
@@ -1,14 +1,16 @@
-function sendAllEmails(userNameList, bloodGroupList, cityList, emailList, seekerName) {
+function sendAllEmails() {
     const successful = [];
     const failed = [];
     let completedRequests = 0;
 
     // Split the values into arrays
-    userName = userNameList.value.split('~');
-    bloodGroup = bloodGroupList.value.split('~');
-    city = cityList.value.split('~');
-    email = emailList.value.split('~');
+    userName = document.forms[0].userNameList.value.split('~');
+    bloodGroup = document.forms[0].bloodGroupList.value.split('~');
+    city = document.forms[0].cityList.value.split('~');
+    email = document.forms[0].emailList.value.split('~');
+    seekerName = document.forms[0].seekerName.value;
 
+    document.getElementById('overlay-loader').style.display = 'flex';
 
     // Iterate through each donor record
     for (let i = 0; i < userName.length; i++) {
@@ -20,7 +22,7 @@ function sendAllEmails(userNameList, bloodGroupList, cityList, emailList, seeker
 
                 A new seeker requires your blood donation assistance. Below are the details:<br>
 
-                Seeker Name: ${seekerName.value}<br>
+                Seeker Name: ${seekerName}<br>
                 Seeker Blood Group: ${bloodGroup[i]}<br>
                 Seeker City: ${city[i]}<br>
 
@@ -45,10 +47,14 @@ function sendAllEmails(userNameList, bloodGroupList, cityList, emailList, seeker
             } else {
                 failed.push(email[i]); // Add failed email to the list
             }
+
+            document.getElementById('overlay-loader').style.display = 'none';
         })
         .catch((error) => {
             console.error("Error sending email to " + userName[i], error);
             // failed.push(email[i]); // If there’s an error, add to the failed list
+
+            document.getElementById('overlay-loader').style.display = 'none';
         })
         .finally(() => {
             completedRequests++;
@@ -56,35 +62,42 @@ function sendAllEmails(userNameList, bloodGroupList, cityList, emailList, seeker
             if (completedRequests == userName.length) {
                 if (failed.length == 0) {
                     alert("Successfully email sent to all the Donars");
+                    document.getElementById('sendMailbtn').className='btnDisable';
+                    document.getElementById('sendMailbtn').disabled='true';
+                    document.getElementById('sendMailbtn').readOnly='true';
                 } else {
                     alert("Failed to send email to the following email IDs: " + failed.join(', '));
                 }
 
             }
+
+            document.getElementById('overlay-loader').style.display = 'none';
         });
     }
 }
 
 
-function donarList_onload(){
-    alert('hello donars');
-    let cleaned = document.forms[0].userNameList.value.replace(/[\[\]',]/g, '');
-    document.forms[0].userNameList.value = cleaned.replace(/\s+/g, '~');
+// function donarList_onload(){
+//     alert('hello donars');
+//     let cleaned = document.forms[0].userNameList.value.replace(/[\[\]',]/g, '');
+//     document.forms[0].userNameList.value = cleaned.replace(/\s+/g, '~');
 
-    let cleaned1 = document.forms[0].bloodGroupList.value.replace(/[\[\]',]/g, '');
-    document.forms[0].bloodGroupList.value = cleaned1.replace(/\s+/g, '~');
+//     let cleaned1 = document.forms[0].bloodGroupList.value.replace(/[\[\]',]/g, '');
+//     document.forms[0].bloodGroupList.value = cleaned1.replace(/\s+/g, '~');
 
-    let cleaned2 = document.forms[0].cityList.value.replace(/[\[\]',]/g, '');
-    document.forms[0].cityList.value = cleaned2.replace(/\s+/g, '~');
+//     let cleaned2 = document.forms[0].cityList.value.replace(/[\[\]',]/g, '');
+//     document.forms[0].cityList.value = cleaned2.replace(/\s+/g, '~');
 
-    let cleaned3 = document.forms[0].emailList.value.replace(/[\[\]',]/g, '');
-    document.forms[0].emailList.value = cleaned3.replace(/\s+/g, '~');
-}
+//     let cleaned3 = document.forms[0].emailList.value.replace(/[\[\]',]/g, '');
+//     document.forms[0].emailList.value = cleaned3.replace(/\s+/g, '~');
+// }
 
 function fetchDonors(){
     var seekername = document.getElementById('seekerName').value;
     var bloodgroup = document.getElementById('bloodgroup').value;
     var city = document.getElementById('city').value;
+
+    document.getElementById('overlay-loader').style.display = 'flex'; // overlay loading screen
             // const params = {
             //     param1: p1,
             //     param2: p2
@@ -102,66 +115,121 @@ function fetchDonors(){
             .then(data => {
                 if(data.username){
                     document.getElementById('fetchedDonars').style.display='block';
-                }
-                // Access the lists from the response
-                const username = data.username;
-                const bloodgroup = data.bloodgroup;
-                const city = data.city;
-                const email = data.email;
+
+                    document.getElementById('fetchDonartDeatails').className='btnDisable';
+                    document.getElementById('fetchDonartDeatails').disabled='true';
+                    document.getElementById('fetchDonartDeatails').readOnly='true';
+
+                    document.getElementById('seekerName').className='btnDisable';
+                    document.getElementById('seekerName').disabled='true';
+                    document.getElementById('seekerName').readOnly='true';
+
+                    document.getElementById('city').className='btnDisable';
+                    document.getElementById('city').disabled='true';
+                    document.getElementById('city').readOnly='true';
+
+                    document.getElementById('bloodgroup').className='btnDisable';
+                    document.getElementById('bloodgroup').disabled='true';
+                    document.getElementById('bloodgroup').readOnly='true';
+
+                    document.getElementById('seekerRequest').className='btnDisable';
+                    document.getElementById('seekerRequest').disabled='true';
+                    document.getElementById('seekerRequest').readOnly='true';
                 
-                var tempUserName = username[0];
-                var tempBloodGroup = bloodgroup[0];
-                var tempCity = city[0];
-                var tempEmail = email[0];
+                    // Access the lists from the response
+                    const username = data.username;
+                    const bloodgroup = data.bloodgroup;
+                    const city = data.city;
+                    const email = data.email;
+                    
+                    var tempUserName = username[0];
+                    var tempBloodGroup = bloodgroup[0];
+                    var tempCity = city[0];
+                    var tempEmail = email[0];
 
-                for(i=0 ; i<username.length ; i++){
-                    tempUserName = tempUserName+'~'+username[i];
-                    tempBloodGroup = tempBloodGroup+'~'+bloodgroup[i];
-                    tempCity = tempCity+'~'+city[i];
-                    tempEmail = tempEmail+'~'+email[i];
+                    for(i=0 ; i<username.length ; i++){
+                        tempUserName = tempUserName+'~'+username[i];
+                        tempBloodGroup = tempBloodGroup+'~'+bloodgroup[i];
+                        tempCity = tempCity+'~'+city[i];
+                        tempEmail = tempEmail+'~'+email[i];
+                    }
+
+                    //local storage for further use-->start
+                    data = {
+                        userName : tempUserName,
+                        bloodGroup : tempBloodGroup,
+                        city : tempCity,
+                        email : tempEmail
+                    }
+                    localStorage.setItem(seekername, JSON.stringify(data));
+
+                    //local storage for further use-->END
+
+                    document.getElementsByName('userNameList')[0].value=tempUserName;
+                    document.getElementsByName('bloodGroupList')[0].value=tempBloodGroup;
+                    document.getElementsByName('cityList')[0].value=tempCity;
+                    document.getElementsByName('emailList')[0].value=tempEmail;
+
+                    // Get the table body element
+                    const tableBody = document.querySelector("#donorDataTable tbody");
+
+                    // Clear the table body first
+                    tableBody.innerHTML = '';
+
+                    // Find the maximum length of the lists (assuming all lists are of equal length)
+                    const maxLength = Math.max(username.length, bloodgroup.length, city.length,email.length);
+
+                    // Loop through the lists and create table rows
+                    for (let i = 0; i < maxLength; i++) {
+                        const row = document.createElement('tr');
+
+                        // Create table cells for each list
+                        const cell1 = document.createElement('td');
+                        cell1.textContent = username[i] || '';  // Use empty string if item doesn't exist
+                        row.appendChild(cell1);
+
+                        const cell2 = document.createElement('td');
+                        cell2.textContent = bloodgroup[i] || '';
+                        row.appendChild(cell2);
+
+                        const cell3 = document.createElement('td');
+                        cell3.textContent = city[i] || '';
+                        row.appendChild(cell3);
+
+                        const cell4 = document.createElement('td');
+                        cell4.textContent = email[i] || '';
+                        row.appendChild(cell4);
+
+                        // Append the row to the table body
+                        tableBody.appendChild(row);
+                    }
                 }
-
-                document.getElementsByName('userNameList')[0].value=tempUserName;
-                document.getElementsByName('bloodGroupList')[0].value=tempBloodGroup;
-                document.getElementsByName('cityList')[0].value=tempCity;
-                document.getElementsByName('emailList')[0].value=tempEmail;
-
-                // Get the table body element
-                const tableBody = document.querySelector("#donorDataTable tbody");
-
-                // Clear the table body first
-                tableBody.innerHTML = '';
-
-                // Find the maximum length of the lists (assuming all lists are of equal length)
-                const maxLength = Math.max(username.length, bloodgroup.length, city.length,email.length);
-
-                // Loop through the lists and create table rows
-                for (let i = 0; i < maxLength; i++) {
-                    const row = document.createElement('tr');
-
-                    // Create table cells for each list
-                    const cell1 = document.createElement('td');
-                    cell1.textContent = username[i] || '';  // Use empty string if item doesn't exist
-                    row.appendChild(cell1);
-
-                    const cell2 = document.createElement('td');
-                    cell2.textContent = bloodgroup[i] || '';
-                    row.appendChild(cell2);
-
-                    const cell3 = document.createElement('td');
-                    cell3.textContent = city[i] || '';
-                    row.appendChild(cell3);
-
-                    const cell4 = document.createElement('td');
-                    cell4.textContent = email[i] || '';
-                    row.appendChild(cell4);
-
-                    // Append the row to the table body
-                    tableBody.appendChild(row);
-                }
+                document.getElementById('overlay-loader').style.display = 'none';
             })
             .catch(error => {
                 console.error('Error:', error);
+
+                document.getElementById('overlay-loader').style.display = 'none';
             });
     return true;
+}
+
+function enablefetchbuttons(){
+    if(document.getElementById('seekerRequest').value =='newRequest'){
+        document.getElementById("fetchDonartDeatails").style.display='block';
+        document.getElementById("fetchSeekerRequestDeatails").style.display='none';
+        document.getElementById("cityBloodFields").style.display='block';
+        document.getElementById("cityBloodFieldsReplace").style.display='none';
+    }
+    else if(document.getElementById('seekerRequest').value=='alreadyRequested'){
+        document.getElementById("fetchDonartDeatails").style.display='none';
+        document.getElementById("fetchSeekerRequestDeatails").style.display='block';
+        document.getElementById("cityBloodFields").style.display='none';
+        document.getElementById("cityBloodFieldsReplace").style.display='block';
+        
+    }
+    else{
+        document.getElementById("fetchDonartDeatails").style.display='none';
+        document.getElementById("fetchSeekerRequestDeatails").style.display='none';
+    }
 }
